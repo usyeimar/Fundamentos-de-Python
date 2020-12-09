26ca85461438c4bc1ccc5d1f75b99a2e1fa5bebb
yeimar <Yeimar112003@outlook.com>
2020-12-09 05:44:10 -0500

    Agrege una nueva clase llamada serpent y le asigne la clase padre Pokemon, ademas de que le agrege un nuevo parametro a la clase padre , el cual es denominado 'Clase'

--- Metodos/Herencia.Uno.py
+++ Metodos/Herencia.Uno.py
@@ -1,11 +1,12 @@
 # Herencia
 class Pokemon: # padre
     pass
-    def __init__(self,nombre,tipo):
+    def __init__(self,nombre,tipo,clase):
             self.nombre = nombre
             self.tipo = tipo
+            self.clase  = clase
     def description (self):
-        return "{} es un pokemon de tipo {}".format(self.nombre,self.tipo)
+        return "{} es un pokemon de tipo {} y pertenece a la clase {}".format(self.nombre,self.tipo,self.clase)
 
 
 class pikachu(Pokemon): #hijo
@@ -16,8 +17,19 @@ class charmander(Pokemon):#hijo
     def ataque(self, tipoataque):
         return "{} tipo de ataque : {} ".format(self.nombre, tipoataque)
 
+class serpent(Pokemon): # Hijo
+    def ataque(self,tipoataque):
+        return "{} tipo de ataque : {} ".format(self.nombre,tipoataque)
+       
 
-nuevo_pokemon = pikachu("electronys","electrico") 
+
+
+nuevo_pokemon = pikachu("electronys","electrico","Caos") 
 print(nuevo_pokemon.description())
-
 print(nuevo_pokemon.ataque(":""electrobalt"))
+
+nuevo_pokemon2 = serpent("lady","agua","Natura")
+print(nuevo_pokemon2.description())
+print(nuevo_pokemon2.ataque(":"'Terremoto'))
+
+
