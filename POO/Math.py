9857cad411daeef57c6b1974750d3616e4c3fa3d
yeimar <Yeimar112003@outlook.com>
2020-12-11 00:36:59 -0500

    Se agrego el archivo math.py que da una breve explicacion sobre la programacion Orientada a Objetos

---
+++ POO/Math.py
@@ -0,0 +1,26 @@
+import math
+from typing import Deque
+
+class Complejo:
+    def __init__(self,real,imaginario) -> None:
+       self.real = real
+       self.img = imaginario
+    
+    def abs(self):
+        print (math.sqrt((self.real*self.real) + (self.img * self.img)))
+
+
+
+def main():
+    numero = Complejo(3,4)  # Se crea el objeto y se inicializa haciendo
+                            # el llamado al metodo __init__ pasnado los valores
+                            # real = 3 y imaginario = 4
+    
+    numero.abs() # Convocamos abs de la clase complejo
+
+input() # Generamos una pausa en la aplicacion esperando a que se presione una tecla
+
+    
+if __name__== '__main__':
+    main()
+
