053d0e4e96ac83d7eed1d71b92933ef6dddc7c17
yeimar <Yeimar112003@outlook.com>
2020-12-11 07:58:16 -0500

    Se agrego el archivo '-' y se confirmaron los cambios

---
+++ j2logo[Python]/Hello_Wordl.py
@@ -0,0 +1,9 @@
+suma = 2+3
+print(suma)
+
+def suma_numeros(numeros):  # Bloque 1
+    suma = 0                # Bloque 2
+    for n in numeros:       # Bloque 2
+        suma += n           # Bloque 3
+        print(suma)         # Bloque 3
+    return suma             # Bloque 2
