a89292b572ac4d77564b222109940eb010f26f0d
yeimar <Yeimar112003@outlook.com>
2020-12-23 06:53:12 -0500

    Se creo una carpeta donde se importan todos mis proyectos en pseint

---
+++ Export[Pseint]/sin_titulo.py
@@ -0,0 +1,16 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	nombre_usuario = str()
+	tiene_armas = bool()
+	num_vidas = int()
+	tiene_armas = True
+	nombre_usuario = "Yeimar"
+	num_vidas = 2
+	print(nombre_usuario)
+	print(tiene_armas)
+	print(num_vidas)
+
